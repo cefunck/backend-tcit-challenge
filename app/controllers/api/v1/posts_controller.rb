5e9a3893cde57d8890ec6fca81013d872ce9b49e
Cristian <cefunck@miuandes.cl>
2024-09-01 05:31:35 -0400

    add create and destroy actions to posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,6 +5,31 @@ module Api
         @posts = Post.all
         render json: @posts
       end
+
+      def create
+        @post = Post.new(post_params)
+
+        if @post.save
+          render json: @post, status: :created, location: api_v1_post_url(@post)
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @post = Post.find(params[:id])
+        if @post.destroy
+          render json: @post, status: :ok
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def post_params
+        params.require(:post).permit(:name, :description)
+      end
     end
   end
 end
