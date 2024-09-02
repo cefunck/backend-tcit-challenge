cd1c01e3ab2096560bc5a164cb354e0e71bce742
Cristian Funck <cefunck@miuandes.cl>
2024-09-02 02:45:43 -0400

    update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-A_VALID_NAME = 'a post name'
-ANOTHER_VALID_NAME = 'another post name'
+A_VALID_NAME = 'post 1'
+ANOTHER_VALID_NAME = 'post 2'
 A_DESCRIPTION = <<~TEXT
   Aute minim occaecat laborum in irure consectetur proident pariatur elit nisi et aliquip.
   Sit anim ut ut et excepteur ea Lorem irure amet enim fugiat.
+TEXT
+ANOTHER_DESCRIPTION = <<~TEXT
   Ut minim ex ipsum reprehenderit consectetur eiusmod minim Lorem quis amet reprehenderit.
   Duis aliquip dolor dolor aliquip dolore magna id deserunt officia exercitation ea aliqua dolor.
 TEXT
@@ -13,7 +15,7 @@ posts = [
   { name: A_VALID_NAME,
     description: A_DESCRIPTION },
   { name: ANOTHER_VALID_NAME,
-    description: A_DESCRIPTION }
+    description: ANOTHER_DESCRIPTION }
 ]
 
 posts.each do |post|
